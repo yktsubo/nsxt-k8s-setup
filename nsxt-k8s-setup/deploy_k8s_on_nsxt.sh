71cdcf3a7bf36f45221b75231ce306cdd9600cd5
Yuki Tsuboi <ytsuboi@vmware.com>
2018-06-15 12:28:08 +0800

    change ip address scheme and tz is nested_nsx

--- nsxt-k8s-setup/deploy_k8s_on_nsxt.sh
+++ nsxt-k8s-setup/deploy_k8s_on_nsxt.sh
@@ -6,6 +6,8 @@ function usage()
     echo "Usage: ${cmdname}"  1>&2
 }
 
+MAX_RETRY=30
+
 # Add hosts file
 echo "${VCENTER_IP} vc-01a.corp.local" >> /etc/hosts
 echo "${NSX_EDGE_01_IP} nsxedge-01a" >> /etc/hosts
@@ -17,7 +19,7 @@ echo "${NSX_CONTROLLER_01_IP} nsxctrl-01a.corp.local" >> /etc/hosts
 echo "${K8S_MASTER_IP} k8s-mater"  >> /etc/hosts
 echo "${K8S_NODE1_IP} k8s-node1"  >> /etc/hosts
 echo "${K8S_NODE2_IP} k8s-node2"  >> /etc/hosts
-echo "${K8S_NODE3_IP} k8s-node2"  >> /etc/hosts
+echo "${K8S_NODE3_IP} k8s-node3"  >> /etc/hosts
 
 # VARIABLE
 cat ./templates/hosts | envsubst > ./hosts
@@ -36,22 +38,33 @@ govc vm.markastemplate "ubuntu"
 # PowerOn VMs
 govc vm.power -on 'nsxedge-02a'
 
-echo "Sleep 180 sec to Wait for Edge node becomes up"
-sleep 180
+echo "Waiting for Edge node to becomes up and running"
+govc vm.info -waitip=true nsxedge-02a > /dev/null
 
 # NSX-T configureation
 pynsxt -c ./pynsxt/connect_to_manager.yml
 
-echo "Sleep 180 sec to Wait for components ready"
-sleep 180
+echo "Sleep 120 sec to Wait for components ready"
+sleep 120
 
 echo "Coniguring NSX-T"
 
 pynsxt -c ./pynsxt/prepare_infra.yml
 pynsxt -c ./pynsxt/prepare_k8s.yml
 
-echo "Sleep 300 sec to Wait for components ready..."
-sleep 300
+n=0
+while :
+do
+    govc find . -type o -name k8s-transport | grep k8s-transport > /dev/null && break
+    n=$[$n+1]
+    sleep 10s
+    if [ $n -ge $MAX_RETRY ]; then
+        echo "failed to find k8s-transport network" >&2
+        exit 1
+    fi
+done
+
+echo "Found k8s-transport network"
 
 # Terraform is used to deploy k8s nodes
 echo "Deploying k8s nodes"
@@ -70,9 +83,13 @@ echo "Host 192.168.*.*" >> ~/.ssh/config
 echo "StrictHostKeyChecking no" >> ~/.ssh/config 
 echo "UserKnownHostsFile=/dev/null"  >> ~/.ssh/config 
 
-
-echo "Sleep 400 sec to Wait for DHCP expired in K8s nodes"
-sleep 400
+K8S_NODES=( "k8s-master" "k8s-node1" "k8s-node2" "k8s-node3" )
+for vm in ${K8S_NODES[@]}
+do
+    echo "Waiting for ${vm} to be ready"
+    govc vm.info -waitip=true ${vm} > /dev/null
+    sleep 3
+done
 
 # Install K8s and NCP by ansible
 echo "Start configuring  k8s nodes"
